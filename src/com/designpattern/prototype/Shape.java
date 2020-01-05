f593ca94fe2862a68d04418ee22f83a79dd32de6
Jang LaeHyuk <navyguy113@gmail.com>
2020-01-05 13:24:07 +0900

    ProtoType Pattern (#4)
    * adapter pattern
    
    * templatemethod pattern
    
    * singleton pattern
    
    * prototype pattern
    
    Co-authored-by: 레오(장래혁) <58159225+lhjang@users.noreply.github.com>

---
+++ src/com/designpattern/prototype/Shape.java
@@ -0,0 +1,14 @@
+package com.designpattern.prototype;
+
+public class Shape implements Cloneable {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId( String id ) {
+        this.id = id;
+    }
+}
